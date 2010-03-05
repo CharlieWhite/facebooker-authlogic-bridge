82bb35b2acf744b757d7964918c4b6071c74d24b
Charlie <charlie@fluidtickets>
2010-03-05 12:50:59 -0500

    added login_with_fb and fb_connect

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ ActionController::Routing::Routes.draw do |map|
   map.logout_fb 'logout_fb_connect', :controller => 'user_sessions', :action => 'logout_facebook_connect'
   
   map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
+ 
   map.resource :account, :controller => "users"
   map.resources :users
+  
+  map.root '/account' # optional, this just sets the root route
    
   # The priority is based upon order of creation: first created -> highest priority.
 
